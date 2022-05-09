2d421369794a15bab35ccede4fc2a637a5f3c8cb
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2022-05-09 21:39:26 +0900

    add title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,14 @@ class Document extends NextDocument<Props> {
     return (
       // https://daisyui.com/docs/themes/
       <Html data-theme="lemonade">
-        <Head />
+        <Head>
+          {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+          <title>Hit And Blow onChain</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
