59d8ec7d784f8c9cd3649bc6e599ca05aebb41c5
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2022-05-09 17:12:58 +0900

    fix hydration mismatch error

--- src/hooks/useChains.ts
+++ src/hooks/useChains.ts
@@ -1,14 +1,23 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { AvailableChains } from "utils";
 
 export const useChains = () => {
   const router = useRouter();
+
   // https://github.com/vercel/next.js/discussions/11484
   const isMainnet = "mainnet" === router.asPath.split(/\?/)[1];
+
+  // https://www.benmvp.com/blog/handling-react-server-mismatch-error/
+  const [isReady, setIsready] = useState(false);
+  useEffect(() => {
+    if (router.isReady) {
+      setIsready(true);
+    }
+  }, [router.isReady]);
+
   return {
     isMainnet,
-    selectedChain: router.isReady
-      ? AvailableChains[isMainnet ? 1 : 0]
-      : undefined,
+    selectedChain: isReady ? AvailableChains[isMainnet ? 1 : 0] : undefined,
   };
 };
